e3324f83a5ae35b7d47d6ecb83ef6a4311eeba51
Mrinal Wadhwa <mrinal@ockam.io>
2023-02-28 16:58:09 -0800

    Generate latest manual version

--- manual/template/mdbook/build.sh
+++ manual/template/mdbook/build.sh
@@ -6,6 +6,7 @@ version="v0.81.0"
 # the build script would look something like this
 
 rm -rf src "$version" "../../$version"
+
 # git clone --depth 1 --branch "ockam_$version" git@github.com:build-trust/ockam.git "$version"
 git clone git@github.com:murex971/ockam.git "$version"
 cd "$version" && git checkout murex971/markdown
@@ -16,6 +17,13 @@ cd ..
 cp SUMMARY.md src/
 
 mdbook build
+
+# move to version folder
 mv book "../../$version"
 
 rm -rf book src "$version"
+
+# make latest
+cd ../..
+GLOBIGNORE='template:v*'; rm -rf *
+cp -R "$version"/. .
